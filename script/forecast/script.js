c88f84793c9865b03b504f0eef61c50a6af1cf8f
gabibarbarin <77581880+gabibarbarin@users.noreply.github.com>
2021-08-18 10:25:04 -0300

    Cambio de API de Geocoding.

--- script/forecast/script.js
+++ script/forecast/script.js
@@ -1,8 +1,3 @@
 import {eventSearchForm} from '../modules.js'
 
 eventSearchForm()
-
-console.log(localStorage.getItem('lat'))
-console.log(localStorage.getItem('lon'))
-console.log(localStorage.getItem('city'))
-console.log(localStorage.getItem('province'))
--- script/modules.js
+++ script/modules.js
@@ -1,30 +1,41 @@
-export const eventSearchForm = () =>{
-    const searchForm = document.querySelector('#search-form')
-
-    searchForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-
-        //window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200")
-        window.location.pathname == '/index.html' ? (window.open("../views/forecast.html"),getCityInfo()) : getCityInfo()
-        //console.log(window.location.pathname) 
+export const getCityInfo = async () => {
+    return new Promise((resolve) => {
+        const city = document.querySelector('#search-input').value
+        
+        const params = {
+            access_key: 'woc8qRJMqbhaEYmNHS8wF2LD8uxemOTW6Bru5-TjfgM',
+            query: `${city}`,
+            language: 'es',
+        }
+        resolve(
+            fetch(`https://geocoder.ls.hereapi.com/6.2/geocode.json?searchtext=${params.query}&gen=9&apiKey=${params.access_key}&language=${params.language}`)
+            .then(response => response.json())
+            .then(data => {
+                localStorage.setItem('lat', data.Response.View[0].Result[0].Location.DisplayPosition.Latitude);
+                localStorage.setItem('lon', data.Response.View[0].Result[0].Location.DisplayPosition.Longitude);
+                localStorage.setItem('city', data.Response.View[0].Result[0].Location.Address.City);
+                localStorage.setItem('province', data.Response.View[0].Result[0].Location.Address.State);
+            })
+        )
     })
 }
 
+export const eventSearchForm = () =>{
+    const searchForm = document.querySelector('#search-form')
 
-export const getCityInfo = () => {
-    const city = document.querySelector('#search-input').value
+    searchForm.addEventListener('submit', async (e) => {
+        e.preventDefault()
 
-    const params = {
-        access_key: '69e1f5b31b492cb5aedf0d78d90dff9f',
-        query: `${city}`,
-    }
-    
-    fetch(`http://api.positionstack.com/v1/forward?access_key=${params.access_key}&query=${params.query}`)
-    .then(response => response.json())
-    .then(data => {
-        localStorage.setItem('lat', data.data[0].latitude);
-        localStorage.setItem('lon', data.data[0].longitude);
-        localStorage.setItem('city', data.data[0].name);
-        localStorage.setItem('province', data.data[0].region);
-    });
+        if(window.location.pathname == '/index.html'){
+            try{
+                await getCityInfo()
+                window.open("../views/forecast.html","_self")
+            }catch(error){
+                console.log(error)
+            }
+            
+        }else{
+            getCityInfo()
+        }
+    })
 }
